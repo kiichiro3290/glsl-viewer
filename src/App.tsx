b9957a50446077986d437f5c39c848098398cdc3
kiichiro3290 <kai.kiichiro.dev@gmail.com>
2025-03-18 05:33:03 +0900

    fix: use utime

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import { useEffect, useRef, useState } from 'react';
 import * as THREE from 'three';
 
-const fragmentShader = `
+const defaultFragmentShader = `
+  precision mediump float;
+
   uniform float uTime;
-  varying vec2 vUv;
+  uniform vec2 uResolution;
 
   void main() {
-    vec3 color = vec3(0.5 + 0.5 * sin(uTime + vUv.x * 10.0),
-                      0.5 + 0.5 * cos(uTime + vUv.y * 10.0),
-                      0.5);
-    gl_FragColor = vec4(color, 1.0);
+      vec2 uv = gl_FragCoord.xy / uResolution.xy;
+      gl_FragColor = vec4(uv, abs(sin(uTime)), 1.0);
   }
 `;
 
@@ -26,6 +26,21 @@ const ShaderPlane = () => {
   const ref = useRef<THREE.Mesh>(null);
   const { size, viewport } = useThree();
   const [scale, setScale] = useState<[number, number, number]>([viewport.width, viewport.height, 1]);
+  const [fragmentShader, setFragmentShader] = useState<string>(defaultFragmentShader);
+
+  useEffect(() => {
+    window.addEventListener('message', (event) => {
+      const message = event.data;
+      if (message?.type === 'updateShader') {
+        console.log('updateShader', message.shader);
+        setFragmentShader(message.shader);
+      }
+    });
+
+    return () => {
+      window.removeEventListener('message', () => {});
+    };
+  }, []);
 
   useEffect(() => {
     setScale([viewport.width, viewport.height, 1]);
@@ -34,7 +49,7 @@ const ShaderPlane = () => {
   // フレームごとに `uTime` を更新
   useFrame(({ clock }) => {
     if (ref.current) {
-      (ref.current.material as any).uTime = clock.getElapsedTime();
+      (ref.current.material as any).uniforms.uTime.value = clock.getElapsedTime();
     }
   });
 
@@ -43,22 +58,13 @@ const ShaderPlane = () => {
       <planeGeometry args={[1, 1]} />
       <shaderMaterial uniforms={{
         uTime: { value: 0.0 },
+        uResolution: { value: new THREE.Vector2(viewport.width, viewport.height) },
       }} fragmentShader={`${fragmentShader}`} vertexShader={`${vertexShader}`} />
     </mesh>
   );
 };
 
 export function App() {
-  useEffect(() => {
-    window.addEventListener('message', (event) => {
-      const _message = event.data;
-    });
-
-    return () => {
-      window.removeEventListener('message', () => {});
-    };
-  }, []);
-
   return (
     <Canvas orthographic camera={{ position: [0, 0, 1], zoom: 1 }}>
       <ShaderPlane />
